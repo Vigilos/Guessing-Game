8e5deee84f40b35adc7d901ec2c85939f8611a37
ERIK M. DODD <emdodd27@gmail.com>
2022-08-17 09:45:18 -0700

    deleted extra comments in app.js

--- app.js
+++ app.js
@@ -62,35 +62,3 @@ let userCityGuess = prompt('Is Paris my favorite city to travel to?');
     // console.log('That\'s correct! Barcelona is actually my favorite city to travel to, ' + user + '.' );
      alert('That\'s correct! Barcelona is actually my favorite city to travel to, ' + user + '.' );
   }
-
-// let color = prompt('What is your favorite color?');
-
-// switch (color) {
-// case 'red':
-//   alert('I like red');
-//   break;
-// case 'blue':
-//   alert('blue is great too');
-//   break;
-// case 'yellow':
-//   alert('yellow is just okay');
-//   break;
-// default:
-//   alert('Oh interesting, I don\'t know that color');
-// }
-
-// // creates a variables and asks the user to give it a value.
-// let movie = prompt('What is one of my favorite action movies?'); // String data type
-// if (typeof(movie) === 'string') {
-//   movie = movie.toLowerCase(); // reassigns movie variable to a lowercase string (if it is already a string).
-// }
-
-// if (movie === 'con air') {
-//   alert('That is a great Movie!');
-// } else if (movie === 'the rock') {
-//   alert('Another great Movie');
-// } else if (movie === 'face off') {
-//   alert('One of my faves!');
-// } else {
-//   alert('I don\'t know that movie!');
-// }
